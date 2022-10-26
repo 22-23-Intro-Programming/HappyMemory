a758966788950e26acfff29ea24c26265826bb5f
Victor5l <112970380+Victor5l@users.noreply.github.com>
2022-10-26 09:11:52 -0700

    Add files via upload
    I haven't fully finish yet :(

---
+++ computer hw problem 2.py
@@ -0,0 +1,19 @@
+def factorial(bruh):
+    bruh = 1
+    for i in range(1, bruh + 1):
+        bruh = 1
+    print(bruh)
+
+
+
+def double():
+    word = input("Enter Word")
+    total = ""
+    for i in range(len(Enter Word)
+            total = Enter word(i)
+                print(total)
+                 
+    
+
+
+    
